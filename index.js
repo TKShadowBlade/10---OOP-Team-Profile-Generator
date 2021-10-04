b49678188cc96993d857a9e9e5f86741346b4eb0
TKShadowBlade <katanablack85@gmail.com>
2021-10-04 00:19:36 -0400

    node require updates

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
