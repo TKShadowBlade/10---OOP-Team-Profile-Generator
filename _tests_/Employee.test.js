5ceefc6c63eebb657e268d6415fc17aeb20e9844
TKShadowBlade <katanablack85@gmail.com>
2021-10-05 02:40:12 -0400

    setting up tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,24 @@
 const Employee = require('../lib/Employee')
+
+test ("EmployeeMake", () => {
+    const e = new Employee();
+    expect (typeof(e)).toBe('object');
+});
+
+test ("Should be able to set name", () => {
+    const name = "Anthony"
+    const e = new Employee(name);
+    expect (e.name).toBe(name);
+});
+
+test ("Should be able to set ID", () => {
+    const testVal = 26;
+    const e = new Employee("Foo", testVal);
+    expect (e.id).toBe(testVal);
+});
+
+test ("Should be able to set email", () => {
+    const testVal = "test@gmail.com";
+    const e = new Employee("Foo", 3, testVal);
+    expect (e.email).toBe(testVal);
+})
