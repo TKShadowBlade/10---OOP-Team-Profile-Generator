9049b785da1649b83d89d07c8835910f4a6bad96
TKShadowBlade <katanablack85@gmail.com>
2022-01-02 16:10:55 -0500

    employee test updates

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -21,4 +21,28 @@ test ("Should be able to set email", () => {
     const testVal = "test@gmail.com";
     const e = new Employee("Foo", 3, testVal);
     expect (e.email).toBe(testVal);
-})
+});
+
+test ("Should be able to get name via getName()", () => {
+    const testVal = "Tony";
+    const e = new Employee(testVal);
+    expect (e.getName()).toBe(testVal);
+});
+
+test ("Should be able to get id via getId()", () => {
+    const testVal = "27";
+    const e = new Employee("Foo", testVal);
+    expect (e.getId()).toBe(testVal);
+});
+
+test ("Should be able to get email via getEmail()", () => {
+    const testVal = "testing@testmail.com";
+    const e = new Employee("Foo", 1, testVal);
+    expect (e.getEmail()).toBe(testVal);
+});
+
+test ("getRole() function should return Employee", () => {
+    const testVal = "Employee";
+    const e = new Employee("Barbara", 1, "testing@testmail.com");
+    expect (e.getRole()).toBe(testVal);
+});
