1a12866cadcb438b89764fc0106a933b5e2b72c0
TKShadowBlade <katanablack85@gmail.com>
2022-01-02 17:05:52 -0500

    updates to inquirer prompt functions

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const inquirer = require('inquirer')
+const path = require("path");
 const fs = require('fs')
 
-const Engineer = require('./lib/Engineer')
-const Manager = require('./lib/Manager')
-const Intern = require('./lib/Intern')
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "managerteam.html")
+
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
 
 const team = [];
+const arrayIds = [];
 
-function questions() {
+function startUp(){
 
-    inquirer.prompt([
-
+  function makeNewManager() { 
+    console.log("You may begin creating your team");  
+    inquirer.prompt([   
         {
             type: "input",
-            name: "managerName",
+            name: "name",
             message: "What is the manager's name?"
         },
         {
             type: "input",
-            name: "managerID",
+            name: "id",
             message: "What is the manager's ID?"
         },
         {
@@ -26,6 +32,19 @@ function questions() {
             name: "email",
             message: "Please enter manager's email address"
         },
-    ])
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office phone number?"
+        }
+    ]).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        team.push(manager);
+        arrayIds.push(answers.id);
+        makeNewTeam();
+    });
 
-}
+    };
+};
+
+startUp();
