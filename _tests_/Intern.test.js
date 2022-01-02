695cecccf949ae0531c0ed40841accbd15c1ad92
TKShadowBlade <katanablack85@gmail.com>
2022-01-02 16:35:08 -0500

    Intern and Manager test files

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,19 @@
 const Intern = require('../lib/Intern')
+
+test ("Can input school info", () => {
+    const testVal = "LSU";
+    const e = new Intern("Foo", 1, "testing@testmail.com", "LSU");
+    expect (e.school).toBe(testVal);
+});
+
+test ('getRole() should return "Intern"', () => {
+    const testVal = "Intern";
+    const e = new Intern("Foo", 1, "testing@testmail.com", "LSU");
+    expect (e.getRole()).toBe(testVal);
+});
+
+test ("Can get school info with getSchool()", () => {
+    const testVal = "LSU";
+    const e = new Intern("Foo", 1, "testing@testmail.com", "LSU");
+    expect (e.getSchool()).toBe(testVal);
+});
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,19 @@
 const Manager = require('../lib/Manager')
+
+test ("Can use constructor to set office number", () => {
+    const testVal = 25;
+    const e = new Manager("Foo", 1, "testing@testmail.com", testVal);
+    expect (e.officeNumber).toBe(testVal);
+});
+
+test ('Can return "Manager" from getrole() function', () => {
+    const testVal = "Manager";
+    const e = new Manager("Foo", 1, "testing@testmail.com", 25);
+    expect (e.getRole()).toBe(testVal);
+});
+
+test ("getOffice() will return office number", () => {
+    const testVal = 25;
+    const e = new Manager("Foo", 1, "testing@testmail.com", testVal);
+    expect (e.getOfficeNumber()).toBe(testVal);
+});
