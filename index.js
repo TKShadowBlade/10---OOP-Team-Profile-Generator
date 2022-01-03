743f722e463a00b5b374ab13465221e1b0090ef0
TKShadowBlade <katanablack85@gmail.com>
2022-01-03 00:30:53 -0500

    prompt functions added

--- index.js
+++ index.js
@@ -44,7 +44,100 @@ function startUp(){
         makeNewTeam();
     });
 
-    };
+  }
+
+  function makeNewTeam() {
+
+    inquirer
+        .prompt([
+            {
+                name: "chooseMember",
+                type: "list",
+                message: "Choose a team member to add:",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "No more team members needed"
+                ]
+            }
+        ]).then(picked => {
+            switch (picked.chooseMember) {
+               case "Engineer":
+                   addEngineer();
+                   break;
+               case "Intern":
+                   addIntern();
+                   break;
+               default:
+                   putTogetherTeam(); 
+            }
+        });
+  }
+
+  function addIntern() {
+      inquirer
+        .prompt([
+            {
+                name: "internName",
+                type: "input",
+                message: "Enter the name of intern",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "At least one character required"
+                }
+            },
+            {
+                name: "internId",
+                type: "input",
+                message: "Enter the intern's ID",
+                validate: answer => {
+                    const valid = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (valid) {
+                        if (arrayIds.includes(answer)) {
+                            return "This ID already in use, please select a new one";
+                        } else {
+                            return true;
+                        }
+                    }
+                    return "ID must be greater than zero";
+                }
+            },
+            {
+                name: "internEmail",
+                type: "input",
+                message: "Enter the intern's email address",
+                validate: answer => {
+                    const valid = answer.match(
+                        /\S+@\S+\.\S+/
+                    );
+                    if (valid) {
+                        return true;
+                    }
+                    return "Email address invalid";
+                }
+            },
+            {
+                name: "internSchool",
+                type: "input",
+                message: "Enter the name of the intern's school",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "At least one character required";
+                }
+            }
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.interId, answers.interEmail, answers.internSchool);
+            team.push(intern);
+            arrayIds.push(answers.internId);
+            makeNewTeam();
+        });
+  }
 };
 
 startUp();
